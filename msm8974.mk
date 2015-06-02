481c38a12a4fc357540de07fbb9d1262cef48b11
Nick Kralevich <nnk@google.com>
2015-06-02 16:00:21 -0700

    msm8974-common: remove persist.sys.usb.config override
    The variable persist.sys.usb.config is no longer honored by the framework,
    and it's presence causes unnecessary toggling of the USB driver, which
    disonnects ADB and makes the device's connection unstable.
    
    Delete it.
    
    Bug: 21404762
    Bug: 18905620
    Change-Id: Ibcc3b6a6f5523695d9daf01e53e8f5ed71ad19fe
    
    Conflicts:
    	msm8974.mk

--- msm8974.mk
+++ msm8974.mk
@@ -62,9 +62,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true
 
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp
-
 # Audio
 PRODUCT_PACKAGES += \
     audiod \
